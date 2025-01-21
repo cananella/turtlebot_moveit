7d9a3a3c0a59bcdf7fbd390186e67ecda1be486f
cananella <97207725+cananella@users.noreply.github.com>
2025-01-21 16:27:51 +0900

    Add files via upload

--- scripts/task.py
+++ scripts/task.py
@@ -14,35 +14,50 @@ def main(args=None):
 
     print ("task start!")
 
-    move_client.target_marker_id = 0
+    # move_client.target_marker_id = 0
     
-    while(move_client.target_marker_distance is None):
-        rclpy.spin_once(move_client)
-        print("waiting for marker")
-        time.sleep(0.1)
-        pass
-    try:
-        while(move_client.target_marker_distance > 0.13):
-            rclpy.spin_once(move_client)
-            print(move_client.target_marker_distance)    
-            move_client.publish_cmd_vel(0.15)
-            time.sleep(0.1)
+    # while(move_client.target_marker_distance is None):
+    #     rclpy.spin_once(move_client)
+    #     print("waiting for marker")
+    #     time.sleep(0.1)
+    #     pass
+    # try:
+    #     while(move_client.target_marker_distance > 0.13):
+    #         rclpy.spin_once(move_client)
+    #         print(move_client.target_marker_distance)    
+    #         move_client.publish_cmd_vel(0.15)
+    #         time.sleep(0.1)
     
-    except:
-        pass
+    # except:
+    #     pass
 
+
+    ### pose 기준으로 움직이기
+    # pose_array = PoseArray()
+    # pose = Pose()
+    # pose.position.x = 0.054273
+    # pose.position.y = -0.081886
+    # pose.position.z = 0.15
+
+    # pose_array.poses.append(pose)
+
+    # response = arm_client.send_request(0, "", pose_array)
+    # arm_client.get_logger().info(f'Response: {response.response}')
 
 
 
     # # # Example usage
     # ##arm control
-    # response = arm_client.send_request(1, "grip_center")
+    # response = arm_client.send_request(1, "home")
     # arm_client.get_logger().info(f'Response: {response.response}')
     
     # ## gripper control
-    # response = arm_client.send_request(2, "open")
-    # arm_client.get_logger().info(f'Response: {response.response}')
+    response = arm_client.send_request(2, "close")
+    arm_client.get_logger().info(f'Response: {response.response}')
 
+    response = arm_client.send_request(9, "")
+    arm_client.get_logger().info(f'Response: {response.response}')
+    
 
     print ("task end!")
     rclpy.shutdown()
